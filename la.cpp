a6d6aa2c4825746bfe0e811216baeeb0b6a8a559
David <67931753+davidolima@users.noreply.github.com>
2023-07-13 14:03:48 -0300

    LA: Fix and comments.

--- la.cpp
+++ la.cpp
@@ -6,6 +6,7 @@
 using namespace la;
 
 Matrix Matrix::mm(Matrix other){
+  // Matrix multiplication. Returns the result of the operation.
   Matrix c (this->rows, other.cols);
   for (int i = 0; i < this->rows; i++){
     for (int j = 0; j < this->cols; j++){
@@ -18,6 +19,7 @@ Matrix Matrix::mm(Matrix other){
 }
 
 void Matrix::add(Matrix other){
+  // In-place addition the elements of matrix "other" to the given matrix.
   for (int i = 0; i < this->rows; i++){
     for (int j = 0; j < this->cols; j++){
       *this->at(i,j) += *other.at(i,j);
@@ -26,8 +28,9 @@ void Matrix::add(Matrix other){
 }
 
 void Matrix::print(){
-    printf("%dx%d matrix\n", this->rows, this->cols);
-    for (int i = 0; i < this->rows; i++){
+  // Prints the matrix.
+  printf("%dx%d matrix\n", this->rows, this->cols);
+  for (int i = 0; i < this->rows; i++){
     printf("| ");
     for (int j = 0; j < this->cols; j++){
       printf("%f ", *this->at(i,j));
@@ -37,5 +40,7 @@ void Matrix::print(){
 }
 
 float* Matrix::at(int i, int j){
-  return &this->elements[i+j*this->rows];
+  // Returns a pointer to the element (i,j) of the given matrix.
+  // Made this way so it's easy to modify the element.
+  return &this->elements[i*this->rows+j];
 }
