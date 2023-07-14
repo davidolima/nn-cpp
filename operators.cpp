dbbf1a573a42f931ad7feb54617e07aadc69ce6d
Igor-Prado-Borja <Igor-Prado-Borja>
2023-07-14 13:27:12 -0300

    Implemented == operator for checking (explicit, not approximate) equality

--- operators.cpp
+++ operators.cpp
@@ -38,3 +38,16 @@ Matrix Matrix::operator-(Matrix other){
 Matrix Matrix::operator*(Matrix other){
   return this->mm(other); // FIXME: change to fast multiplication
 }
+
+bool Matrix::operator==(Matrix other){
+  // same size
+  bool eq = (this->rows == other.rows) && (this->cols == other.cols);
+
+  // same elements
+  for (int i = 0; i < this->rows; i++){
+    for (int j = 0; j < this->cols; j++){
+      eq = eq && (*(this->at(i, j)) == *(other.at(i, j)));
+    }
+  }
+  return eq;
+}
