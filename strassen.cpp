85d9a3b7b33d13f26f3342111f0730d5143ec3e5
IgorPBorja <IgorPBorja>
2023-07-15 13:44:10 -0300

    Fixed mistake in filling algorithm. Fixed typo in Strassen matrix multiplication main function

--- strassen.cpp
+++ strassen.cpp
@@ -7,8 +7,18 @@ using namespace la;
 // using namespace strassen;
 
 Matrix strassen::fill_to_power2(Matrix m){
-  int new_rows = (int)(ceil(log2(m.rows)));
-  int new_cols = (int)(ceil(log2(m.cols)));
+  auto get_geq_power2 = [](int x){
+    if (x <= 1){
+      return 1;
+    } else{
+      // __builtin_clz = leading 0s in binary representation
+      long unsigned int ceil_log2 = sizeof(int) * 8  -__builtin_clz(x - 1);
+      return 1 << ceil_log2;
+    }
+  };
+
+  int new_rows = get_geq_power2(m.rows);
+  int new_cols = get_geq_power2(m.cols);
 
   Matrix fill_matrix(new_rows, new_cols);
 
@@ -48,38 +58,42 @@ strassen::BlockMatrix::BlockMatrix(Matrix m){
       *(this->topleft.at(i, j)) = *filled_m.at(i, j);
     }
     for (int j = c / 2; j < c; j++){
-      *(this->topright.at(i, j)) = *filled_m.at(i, j);
+      *(this->topright.at(i, j - c/2)) = *filled_m.at(i, j);
     }
   }
   for (int i = r / 2; i < r; i++){
     for (int j = 0; j < c / 2; j++){
-      *(this->bottomleft.at(i, j)) = *filled_m.at(i, j);
+      *(this->bottomleft.at(i - r/2, j)) = *filled_m.at(i, j);
     }
     for (int j = c / 2; j < c; j++){
-      *(this->bottomright.at(i, j)) = *filled_m.at(i, j);
+      *(this->bottomright.at(i - r/2, j - c/2)) = *filled_m.at(i, j);
     }
   }
 }
 
 Matrix strassen::BlockMatrix::join(){
-  Matrix m(this->topleft.rows + this->bottomleft.rows,
-            this->topleft.cols + this->topright.cols);
+  int r1 = this->topleft.rows,
+    r2 = this->bottomleft.rows,
+    c1 = this->topleft.cols,
+    c2 = this->topright.cols;
 
-  for (int i = 0; i < this->topleft.rows; i++){
-    for (int j = 0; j < this->topleft.cols; i++){
+  Matrix m(r1 + r2, c1 + c2);
+
+  for (int i = 0; i < r1; i++){
+    for (int j = 0; j < c1; j++){
       *m.at(i, j) = *(this->topleft.at(i, j));
     }
-    for (int j = 0; j < this->topright.cols; j++){
-      *m.at(i, j) = *(this->topright.at(i, j));
+    for (int j = c1; j < c1 + c2; j++){
+      *m.at(i, j) = *(this->topright.at(i, j - c1));
     }
   }
 
-  for (int i = 0; i < this->bottomleft.rows; i++){
-    for (int j = 0; j < this->bottomleft.cols; j++){
-      *m.at(i + this->topleft.rows, j) = *(this->bottomleft.at(i, j));
+  for (int i = r1; i < r1 + r2; i++){
+    for (int j = 0; j < c1; j++){
+      *m.at(i, j) = *(this->bottomleft.at(i - r1, j));
     }
-    for (int j = 0; j < this->bottomright.cols; j++){
-      *m.at(i + this->topleft.rows, j + this->bottomleft.cols) = *(this->bottomright.at(i, j));
+    for (int j = c1; j < c1 + c2; j++){
+      *m.at(i, j) = *(this->bottomright.at(i - r1, j - c1));
     }
   }
   return m;
@@ -87,14 +101,22 @@ Matrix strassen::BlockMatrix::join(){
 
 
 Matrix strassen::strassen_mm(Matrix A, Matrix B){
+  assert(A.cols == B.rows);
   if (A.rows < min_size && A.cols < min_size && B.rows < min_size && B.cols < min_size){
     return A * B;
   }
+  // Another possibility: allow recursion up to base case n=m=1
+  // Might be slow (Strassen's algorithm is slower than naive O(n^3) for n small, because of large constant factor and hidden terms)
+  // if (A.rows == 1 && A.cols == 1 && B.rows ==  1 && B.cols == 1){
+  //   Matrix m(1, 1);
+  //   *m.at(0,0) = *A.at(0, 0) * *B.at(0, 0);
+  //   return m;
+  // }
 
-  BlockMatrix BlockA = BlockMatrix(A); 
+  BlockMatrix BlockA = BlockMatrix(A);
   BlockMatrix BlockB = BlockMatrix(B);
 
-  Matrix A11 = BlockA.topleft; 
+  Matrix A11 = BlockA.topleft;
   Matrix A12 = BlockA.topright; 
   Matrix A21 = BlockA.bottomleft; 
   Matrix A22 = BlockA.bottomright;
@@ -103,11 +125,12 @@ Matrix strassen::strassen_mm(Matrix A, Matrix B){
   Matrix B21 = BlockB.bottomleft; 
   Matrix B22 = BlockB.bottomright;
 
+
   Matrix M1 = strassen_mm((A11 + A22), (B11 + B22));
   Matrix M2 = strassen_mm((A21 + A22), B11);
   Matrix M3 = strassen_mm(A11, (B12 - B22));
   Matrix M4 = strassen_mm(A22, (B21 - B11));
-  Matrix M5 = strassen_mm((A11 + A22),  B22);
+  Matrix M5 = strassen_mm((A11 + A12),  B22);
   Matrix M6 = strassen_mm((A21 - A11),  (B11 + B12));
   Matrix M7 = strassen_mm((A12 - A22), (B21 + B22));
 
@@ -115,6 +138,13 @@ Matrix strassen::strassen_mm(Matrix A, Matrix B){
                                   M3 + M5,
                                   M2 + M4,
                                   M1 - M2 + M3 + M6);
-  return BlockC.join();
+  Matrix filled_ans = BlockC.join();
+  Matrix ans(A.rows, B.cols);
+  for (int i = 0; i < A.rows; i++){
+    for (int j = 0; j < B.cols; j++){
+      *(ans.at(i, j)) = *(filled_ans.at(i, j));
+    }
+  }
+  return ans;
 }
 
