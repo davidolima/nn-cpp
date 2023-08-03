ee9970722ab31a2bbc2aa0e7bf30ee1c00223a20
David <67931753+davidolima@users.noreply.github.com>
2023-08-02 22:59:45 -0300

    Remove prints from test.

--- tests.cpp
+++ tests.cpp
@@ -24,26 +24,9 @@ bool readme(){
   C = C * I;
   D = D * I;
 
-  printf("A = ");
-  A.print();
-  printf("B = ");
-  B.print();
-  printf("C = ");
-  C.print();
-  printf("D = ");
-  D.print();
-  printf("I = ");
-  I.print();
-
-  printf("det(A) = %f, det(B) = %f, det(C) = %f, det(D) = %f\n", A.det(), B.det(), C.det(), D.det());
-  printf("tr(A) = %f, tr(B) = %f, tr(C) = %f, tr(D) = %f\n", A.trace(), B.trace(), C.trace(), D.trace());
-
   Matrix E = la::generic(2,2);
-  printf("A = E? ");
-  if (A == E){
-    printf("true\n");
-  } else {
-    printf("false\n");
+  if (A != E){
+    return false;
   }
 
   return true;
