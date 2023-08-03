f8b397ba8944a163a3fcf39588e7e3f21c2c87de
David <67931753+davidolima@users.noreply.github.com>
2023-08-02 22:54:36 -0300

    Update README and add README example to tests.

--- tests.cpp
+++ tests.cpp
@@ -5,6 +5,50 @@
 using namespace nn;
 using namespace la;
 
+bool readme(){
+  printf("%s:", __func__);
+  Matrix A = {
+    {1, 2},
+    {3, 4}
+  };
+
+  Matrix B = A.transpose();
+
+  Matrix C = A + B;
+  Matrix D = A * B;
+
+  Matrix I = la::identity(2);
+
+  A = A * I;
+  B = B * I;
+  C = C * I;
+  D = D * I;
+
+  printf("A = ");
+  A.print();
+  printf("B = ");
+  B.print();
+  printf("C = ");
+  C.print();
+  printf("D = ");
+  D.print();
+  printf("I = ");
+  I.print();
+
+  printf("det(A) = %f, det(B) = %f, det(C) = %f, det(D) = %f\n", A.det(), B.det(), C.det(), D.det());
+  printf("tr(A) = %f, tr(B) = %f, tr(C) = %f, tr(D) = %f\n", A.trace(), B.trace(), C.trace(), D.trace());
+
+  Matrix E = la::generic(2,2);
+  printf("A = E? ");
+  if (A == E){
+    printf("true\n");
+  } else {
+    printf("false\n");
+  }
+
+  return true;
+}
+
 bool matrix_creation(){
   printf("%s:", __func__);
   std::vector<std::vector<float>> elements = {{1,2,3},{4,5,6}, {7,8,9}};
@@ -217,6 +261,7 @@ bool matrixMultiplication(){
 
 int main(){
   bool (*tests[])() = {
+    readme,
     determinant,
     matrixMultiplication,
     submat,
